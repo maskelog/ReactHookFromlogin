2de9c649ad86b2245e15580c1e8053c574ba014e
maskelog <blitzmaske@gmail.com>
2024-02-02 23:37:26 +0900

    react hook form 학습

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import LoginForm from "./LoginForm";
+
+export default function App() {
+  return (
+    <>
+      <header className="container">
+        <h2>React Hook Form 로그인 폼</h2>
+      </header>
+      <main className="container">
+        <LoginForm />
+      </main>
+    </>
+  );
+}
---
+++ src/LoginForm.jsx
@@ -0,0 +1,29 @@
+import { useForm } from "react-hook-form";
+
+function LoignForm() {
+  const { register, handleSubmit } = useForm();
+  return (
+    <form
+      noValidate
+      onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}
+    >
+      <label htmlFor="email">이메일</label>
+      <input
+        id="email"
+        type="email"
+        placeholder="test@email.com"
+        {...register("email")}
+      />
+      <label htmlFor="password">비밀번호</label>
+      <input
+        id="password"
+        type="password"
+        placeholder="******"
+        {...register("password")}
+      />
+      <button type="submit">로그인</button>
+    </form>
+  );
+}
+
+export default LoignForm;
